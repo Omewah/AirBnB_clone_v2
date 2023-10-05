7b85bd60df8a28809d0003a4fdae291e185e5d87
Omewah <joelomewah@ymail.com>
2023-10-05 19:54:57 +0100

    distrubutes archived contents to the web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,11 @@
 #!/usr/bin/python3
 """a script that distributes an archive to the web servers"""
 import os
-from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+from fabric.api import env, run, put
 
 
 env.hosts = ["34.203.77.75", "18.204.8.189"]
 """web server 01 and 02 IP addresses."""
-
-
-@runs_once
-def do_pack():
-    """archive the contents of the web_static folder"""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    current_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        current_time.year,
-        current_time.month,
-        current_time.day,
-        current_time.hour,
-        current_time.minute,
-        current_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
 
 
 def do_deploy(archive_path):
