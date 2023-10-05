9b85c607b757ed62b5cbbeb23f24d73d9e25e95b
Omewah <joelomewah@ymail.com>
2023-10-05 19:17:48 +0100

    archives web_static contents

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""a Fabric script that generates a .tgz archive from the contents of the web_static"""
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
+
+
+@runs_once
+def do_pack():
+    """archive the contents of the web_static folder"""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    current_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        current_time.year,
+        current_time.month,
+        current_time.day,
+        current_time.hour,
+        current_time.minute,
+        current_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
